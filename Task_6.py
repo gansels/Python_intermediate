da63f0878d2ec4699f01ef0b8fa8d7419e776791
Ganesh <gsduraisamy@gmail.com>
2024-11-13 17:35:00 +0100

    task 6 to connect to github including error tasks

--- Task_6.py
+++ Task_6.py
@@ -17,3 +17,32 @@ print("Total weight of both animals:" , Animals.total_weight)
 animals2.set_weight(7)
 print("Total weight of animals after change : ", Animals.total_weight)
 
+#Adding abstract method to base class
+from abc import ABC, abstractmethod
+
+class Animals(ABC):
+    total_weight = 0
+
+    def __init__(self,weight):
+        self.weight = weight
+        Animals.total_weight += weight
+
+    @abstractmethod
+
+    def set_weight(self, new_weight):
+        pass
+
+    #Try to create an instance to make error
+    try:
+        animal = Animals(20)
+    except TypeError as m:
+        print(m)
+class Cat(Animals):
+    def set_weight(self, new_weight):
+        Animals.total_weight -= self.weight
+        self.weight = new_weight
+        Animals.total_weight += new_weight
+cat = Cat(15)
+Cat1 = Cat(25)
+
+print("Animal.total_weight")
