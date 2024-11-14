5b5f93986acb257d01e607ea5389e8d6ef6df95b
Ganesh <gsduraisamy@gmail.com>
2024-11-14 19:18:22 +0100

    add to github

--- task_8_b-time.py
+++ task_8_b-time.py
@@ -1,5 +1,24 @@
+#import time
+# start = time.time()
+# time.sleep(1)
+# end = time.time()
+# print(f"Uplynulý čas {end - start:.4f}")
 import time
-start = time.time()
-time.sleep(1)
-end = time.time()
-print(f"Uplynulý čas {end - start:.4f}")
+#decorator to create time and function using import time
+def func_time(function):
+    def control(*args, **kwargs):
+        start = time.time()
+        result = function(*args, **kwargs)
+        end = time.time()
+        print(f"Uplynulý čas {end - start:.4f} sec")
+        return result
+    return control
+#creating decorator
+@func_time
+def create_func_time():
+    print(f" this is functioning: ")
+    time.sleep(1)
+# Call the function
+create_func_time()
+
+
