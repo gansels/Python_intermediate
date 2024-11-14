63a092b9b812af185ae27c0d1a97db7d3fd5374e
Ganesh <gsduraisamy@gmail.com>
2024-11-14 20:18:35 +0100

    add to github

--- Taks_8_a.py
+++ Taks_8_a.py
@@ -1,18 +1,17 @@
-def create_password(func):
-    def user(* args, **kwargs):
-        correct = "name"
-        enter_password = input("Enter password: ")
-        if enter_password == correct:
-            return func(* args, **kwargs)
-        else:
-            print("Wrong passowrd")
-            return None
-    return user
-@create_password
+def create_password(password):
+    def decor(func):
+        def user():
+            user_password = input("Enter password: ")
+            if user_password == password:
+                return func()
+            else:
+                print("Wrong passowrd")
+        return user
+    return decor
+@create_password("name1213")
 def perfect_pass():
     print("The Password is correct, Open it")
 
 perfect_pass()
-
 
 
