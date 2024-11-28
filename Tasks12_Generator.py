49f445dfcfa036fe1427d1163c48df159dea87eb
Ganesh <gsduraisamy@gmail.com>
2024-11-28 19:25:52 +0100

    add to github

--- Tasks12_Generator.py
+++ Tasks12_Generator.py
@@ -7,11 +7,11 @@
 # "Hu", "Nor", "Gil"
 
 def Generator_jmeno(pred, konc):
-    for p in pred:
-        yield p + konc
+    for i in pred:
+        yield i + konc
 
 
-predpony = ["Ga", "Di", "Ja", "Ho", "Vigh"]
-koncovka = "nesh"
+predpony = ["Ga", "Di", "Ja", "Ho", "Vigh"] #predpony-prefix
+koncovka = "nesh" # koncovka- suffix
 for jmeno in Generator_jmeno(predpony, koncovka):
     print(f" Tve jmeno je {jmeno}")
